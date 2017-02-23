ef4d1cb6a4b1a19abcc228ef74f9210ea0c86a5d
Michael McDuffee <mrmcduff@stripe.com>
2017-02-23 13:25:01 -0800

    adding dynamic routing with a bit of magic (end of section 5)

--- angular-rails/freelance-camp-fe/app/app.module.ts
+++ angular-rails/freelance-camp-fe/app/app.module.ts
@@ -7,6 +7,7 @@ import { DocumentsComponent } from './documents/documents.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -21,7 +22,8 @@ import { AppRoutingModule } from './app-routing.module';
     DocumentsComponent,
     HomepageComponent,
     ProposalListComponent,
-    ProposalNewComponent
+    ProposalNewComponent,
+    ProposalShowComponent
   ],
   bootstrap: [
     AppComponent

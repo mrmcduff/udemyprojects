ac61303a319fca45b4e651b64e4bbe3d422ef799
Michael McDuffee <mrmcduff@stripe.com>
2017-03-03 18:20:57 -0800

    rails proposal microservice now working - end l51

--- angular-rails/freelance_camp_proposal/db/seeds.rb
+++ angular-rails/freelance_camp_proposal/db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |proposal|
+Proposal.create!(
+  customer: "Customer #{proposal}",
+  portfolio_url: 'http://portfolio.jordanhudgens.com',
+  tools: 'Ruby on Rails, Angular 2, and Postgres',
+  estimated_hours: (80 + proposal),
+  hourly_rate: 120,
+  weeks_to_complete: 12,
+  client_email: 'jordan@devcamp.com'
+)
+end
+
+# rails g scaffold Proposal customer:string portfolio_url:string tools:string estimated_hours:decimal hourly_rate:decimal weeks_to_complete:integer client_email:string
